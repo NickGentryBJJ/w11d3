be9cba352bd1cef7414e6a1a46d608756534d45e
nj <nickgentrymma@gmail.com>
2023-06-07 12:21:35 -0700

    finished phases 0-3

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/context/ClimateContext.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/context/ClimateContext.js
@@ -1,2 +1,24 @@
+import { createContext, useContext, useState } from 'react';
+
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+
+export const ClimateContext = createContext();
+
+export const useClimate = useContext(ClimateContext)
+
+export default function climateProvider({children}) {
+    const [temp, setTemp] = useState(50);
+
+    return (
+        <ClimateContext.Provider
+        value={{
+            temp,
+            setTemp
+        }}>
+            {children} Question for after lunch...Why is this here?
+            what is it doing and where does it come from?
+        </ClimateContext.Provider>
+    )
+
+}
